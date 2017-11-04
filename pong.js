ffeef53d212ca4aafb4e2b5c61eb377e51279911
Liam <Liam@Liams-MacBook.local>
2017-11-04 16:03:14 -0700

    complete this little project

--- pong.js
+++ pong.js
@@ -1,19 +1,34 @@
 function getNumber() {
   var input = parseInt(prompt ("enter a number greater than zero"));
+  $("#ping-pong").show();
   return (input);
 
 };
 
 $(document).ready(function () {
 
+var numbers1 = [];
+var numbers2 = [];
 
 
-  $("h1").click(function(){ var userInt = getNumber();
+  $("h1").click(function(){
+
+    var userInt = getNumber();
     alert( "var is " + userInt );
-  });
+    for (var i = 1; i <= userInt ; i++) { numbers1.push(i)
+    }
+    numbers1.forEach(function(i) {  $("#numbers").append("<li>" + i + "</li>")
+      });
+    });
 
-  $("#numbers").click(function(){ alert( "this is the list");
+    $("ping-pong").click(function(){
+        // clear list,
+        // do math,
+        // populate new list,
 
-  });
+    });
+
+
+
 
 });
