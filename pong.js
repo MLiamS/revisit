10d2e79bbe66f1ab76b12f890e3aa145cef9ba42
Liam <Liam@Liams-MacBook.local>
2017-11-04 16:03:33 -0700

    test

--- pong.js
+++ pong.js
@@ -1,34 +1,37 @@
 function getNumber() {
   var input = parseInt(prompt ("enter a number greater than zero"));
   $("#ping-pong").show();
+  $(".ping-pong").show();
   return (input);
-
 };
 
 $(document).ready(function () {
 
 var numbers1 = [];
-var numbers2 = [];
+var b = "<button id=restart type=button name=button>RESTART!</button>";
 
-
-  $("h1").click(function(){
+  $("#start").click(function(){
 
     var userInt = getNumber();
-    alert( "var is " + userInt );
     for (var i = 1; i <= userInt ; i++) { numbers1.push(i)
     }
     numbers1.forEach(function(i) {  $("#numbers").append("<li>" + i + "</li>")
       });
     });
 
-    $("ping-pong").click(function(){
-        // clear list,
-        // do math,
-        // populate new list,
+    $("#ping-pong").click(function(){
+      $("#numbers").empty();
+          numbers1.forEach(function(i){
+            if ((i % 15) === 0) numbers1.splice(i-1, 1, "ping-pong");
+            else if ((i % 5) === 0) numbers1.splice(i-1, 1, "pong");
+            else if ((i % 3) === 0) numbers1.splice(i-1, 1, "ping");
+          });
 
+            numbers1.forEach(function(i) {  $("#numbers").append("<li>" + i + "</li>")
+        });
+        $("#numbers").append(b);
+        $("#restart").click(function(){
+          location.reload();
+        });
+      });
     });
-
-
-
-
-});
